72bf3c3cb18cba4f24563b9eb5a6ff7439b110de
joynalabedin05 <joynal05101993@gmail.com>
2023-08-30 10:42:47 +0600

    hellow php

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>frelancer nasim</title>
+</head>
+<body>
+    
+    <?php 
+        // $num1 = 10;
+        // $num2 = 10;
+        // echo $num1+$num2;
+        // $fname = 'joynal';
+        // $lname = 'abedin';
+        // echo $fname.' '.$lname;
+        // $x = 10;
+        // $y = 10;
+        // $z = $x+$y;
+        // echo $z;
+        // $name = 'joynal';
+        // $age = 30;
+        // echo 'my name is:'. $name. '<br> my age is:'. $age;
+
+        // $GLOBALS['name'] = 'joynal';
+        // echo $name;
+
+        // $x = 5985;
+        // $x = 'errr';
+        // var_dump($x);
+        // $x = 'joynal';
+        // $num1 = 'hellow world';
+        // echo strlen($x);
+        // echo str_word_count($num1);
+        // echo strrev($num1);
+        // echo strpos($num1,'world');
+        // echo str_replace ('world', 'nasim', $num1);
+
+        // define('pass', 'joynal0510');
+        // echo pass;
+
+        // $num1 = 33;
+        // // $num2 = 12;
+
+        // if($num1 <= 10){
+        //     echo('apni sotti bolcen');
+        // }
+        // elseif($num1 <= 19){
+        //     echo('apni mitha bolcewn');
+        // }
+        // else{
+        //     echo('apnar boyos bis er upore');
+        // }
+
+        // $name = '';
+
+        // switch($name){
+        //     case "joynal":
+        //     echo('web developer');
+        //     break;
+        //     case "nasim":
+        //     echo('frelancer');
+        //     break;
+        //     default:
+        //     echo('nothing');    
+        // }
+
+        // $x = 1;
+        // while($x<=100){
+        //     echo("the number is:".$x."<br>");
+        //     ++$x;
+        // }
+
+        // for($x = 1; $x<=100; ++$x){
+        //     echo("the number is:".$x."<br>");
+        // }
+
+        // $x = 1;
+        // $myarray = array('abir', 'bablu', 'tamin','rafiq', 'nilu');
+        // sort($myarray);
+        // foreach ($myarray as $name) {
+        //     echo($x.'. '.$name.'<br>');
+        //     $x++;
+        // }
+
+        // $name= 'joynal';
+        // function cal($a,$b){
+        //     global $name;
+        //     echo($name .'<br>'.$a+$b);
+        // }
+        // cal(2,3);
+
+        // $myarray = array('hellow', 'gellow', 'tellow');
+        // // echo $myarray[0];
+        // // echo count($myarray);
+        // $arraycount =  count($myarray);
+
+        // for($x=0; $x < $arraycount; ++$x){
+        //     echo $myarray[$x].'<br>';
+        // }
+
+        // $myarray = array('name'=>'nasim', 'profession'=>'frelancer', 'age'=>'22');
+        // // echo $myarray['name'];
+        // foreach($myarray as $myindex=>$value){
+        //     echo $myindex.': '.$value. '<br>';
+        // }
+
+        echo $_SERVER['HTTP_USER_AGENT'];
+        
+
+     
+    ?>
+</body>
+</html>
